05e3fca642b42812053322e36eff0ec599cb870f
AhrorSulaymonov <ahrorsulaymonov494@gmail.com>
2025-01-17 13:38:25 +0500

    second commit

--- routes/admins.routes.js
+++ routes/admins.routes.js
@@ -10,6 +10,7 @@ const {
   refreshAdminToken,
 } = require("../controllers/admin.controller");
 const admin_police = require("../police_middleware/admin_police");
+const admin_self_police = require("../police_middleware/admin_self_police");
 const creator_police = require("../police_middleware/creator_police");
 
 const router = require("express").Router();
@@ -18,7 +19,7 @@ router.post("/create", creator_police, addAdmin);
 router.get("/all", admin_police, findAllAdmins);
 router.get("/:id", admin_police, findAdminById);
 router.delete("/:id", creator_police, deleteAdminById);
-router.put("/:id", updateAdminById);
+router.put("/:id", admin_police, admin_self_police, updateAdminById);
 
 router.get("/activate/:link", activateAdmin);
 router.post("/login", loginAdmin);
